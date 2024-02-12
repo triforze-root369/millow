61f9d0e591276a8b583ab70b022eee1c9311d4b4
triforze-root369 <triforze.root369@gmail.com>
2024-02-12 15:54:32 -0500

    more logic more tests

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -15,6 +15,16 @@ contract Escrow {
     address public inspector;
     address public lender;
 
+    modifier onlySeller() {
+        require(msg.sender == seller, "Only seller can call this method");
+        _;
+    }
+
+    mapping(uint256 => bool) public isListed;
+    mapping(uint256 => uint256) public purchasePrice;
+    mapping(uint256 => uint256) public escrowAmount;
+    mapping(uint256 => address) public buyer;
+
     constructor(
         address _nftAddress, 
         address payable _seller, 
@@ -25,5 +35,19 @@ contract Escrow {
         seller = _seller;
         inspector = _inspector;
         lender = _lender;
+    }
+
+    // transfer NFT from seller to this contract
+    function list(
+        uint256 _nftID, 
+        address _buyer, 
+        uint256 _purchasePrice, 
+        uint256 _escrowAmount
+    ) public payable onlySeller {
+        IERC721(nftAddress).transferFrom(msg.sender, address(this), _nftID);
+        isListed[_nftID] = true;
+        purchasePrice[_nftID] = _purchasePrice;
+        escrowAmount[_nftID] = _escrowAmount;
+        buyer[_nftID] = _buyer;
     }
 }
