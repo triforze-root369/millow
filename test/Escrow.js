ab161d78cd3915511bcf8b60e3533c77c1fd1037
triforze-root369 <triforze.root369@gmail.com>
2024-02-12 00:57:29 -0500

    some logic plus some tests

--- test/Escrow.js
+++ test/Escrow.js
@@ -6,5 +6,21 @@ const tokens = (n) => {
 }
 
 describe('Escrow', () => {
+    
+    // means of testing if one can save the escrow contract address
+    it('saves the addresses', async() => {
 
+        // gets the contract factory
+        const RealEstate = await ethers.getContractFactory('RealEstate')
+        // deploy the contract
+        realEstate = await RealEstate.deploy();
+
+        // log the address
+        //console.log(realEstate.address)
+
+        // mint
+        let transaction = await realEstate.mint();
+
+        // https://ipfs.io/ipfs/QmTudSYeM7mz3PkYEWXWqPjomRPHogcMFSq7XAvsvsgAPS
+    })
 })
